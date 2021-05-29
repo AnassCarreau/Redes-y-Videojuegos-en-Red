af245097bd9dbedb2a64675e102947c442bad72f
AnassCarreau <47498100+AnassCarreau@users.noreply.github.com>
2021-05-29 18:47:51 +0200

    Serializacion terminada

--- practica2.2/serializacion/ejercicio1.cc
+++ practica2.2/serializacion/ejercicio1.cc
@@ -1,7 +1,7 @@
 #include "Serializable.h"
 
 #include <iostream>
-#include <string>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -9,34 +9,58 @@
 #include <string.h>
 #include <unistd.h>
 
-class Jugador: public Serializable
+#define MAX_NLENGHT 80
+
+class Jugador : public Serializable
 {
 public:
-    Jugador(const char * _n, int16_t _x, int16_t _y):x(_x),y(_y)
+    Jugador(const char *_n, int16_t _x, int16_t _y) : x(_x), y(_y)
     {
-        strncpy(name, _n, 80);
+        strncpy(name, _n, MAX_NLENGHT);
     };
 
     virtual ~Jugador(){};
 
     void to_bin()
     {
-        //
-        int32_t data_size=
-        memccpy(_data,data,data_size);
-        memccpy()
-        memccpy()
+        //tamaño de la clase
+        int32_t data_size = MAX_NLENGHT * sizeof(char) + 2 * sizeof(int16_t);
+        //reserva memoria
+        alloc_data(data_size);
+        //Puntero que apunte a la direccion de memoria
+        char *tmp = _data;
+        //Copiar nombre a partir de direccion
+        memcpy(tmp, name, MAX_NLENGHT * sizeof(char));
+        //Avanzar puntero
+        tmp += MAX_NLENGHT * sizeof(char);
+        //Copiar x a partir de direccion
+        memcpy(tmp, &x, sizeof(int16_t));
+        //Avanzar puntero
+        tmp += sizeof(int16_t);
+        //Copiar y a partir de direccion
+        memcpy(tmp, &y, sizeof(int16_t));
     }
 
-    int from_bin(char * data)
+    int from_bin(char *data)
     {
-        //
+        //Puntero que apunte a la direccion de memoria
+        //para copiar
+        char *tmp = data;
+        //Copiar nombre
+        memcpy(name, tmp, MAX_NLENGHT * sizeof(char));
+        //Avanzar puntero
+        tmp += MAX_NLENGHT * sizeof(char);
+        //Copiar entero x
+        memcpy(&x, tmp, sizeof(int16_t));
+        //Avanzar puntero
+        tmp += sizeof(int16_t);
+        //Copiar entero y
+        memcpy(&y, tmp, sizeof(int16_t));
         return 0;
     }
 
-
 public:
-    char name[80];
+    char name[MAX_NLENGHT];
 
     int16_t x;
     int16_t y;
@@ -48,11 +72,23 @@ int main(int argc, char **argv)
     Jugador one_w("Player_ONE", 123, 987);
 
     // 1. Serializar el objeto one_w
+    one_w.to_bin();
     // 2. Escribir la serialización en un fichero
+    int fd = open("./data_jugador.txt", O_CREAT | O_TRUNC | O_RDWR, 0666);
+    ssize_t size = write(fd, one_w.data(), one_w.size());
+    close(fd);
     // 3. Leer el fichero
+    fd = open("./data_jugador.txt", O_RDONLY, 0666);
+    char buffer[size];
+    if (read(fd, buffer, size) == -1)
+    {
+        return -1;
+    };
+    close(fd);
     // 4. "Deserializar" en one_r
+    one_r.from_bin(buffer);
     // 5. Mostrar el contenido de one_r
+    std::cout << "Nombre: " << one_r.name << "\nx: " << one_r.x << "\ny: " << one_r.y << "\n";
 
     return 0;
 }
-
