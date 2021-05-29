86f8f7773b8c7b656f8e9f5cdad77bfa9c2edd43
AnassCarreau <47498100+AnassCarreau@users.noreply.github.com>
2021-05-30 00:11:38 +0200

    Ejercicio 5

--- practica2.2/replicacion-chat/Chat.cc
+++ practica2.2/replicacion-chat/Chat.cc
@@ -10,16 +10,33 @@ void ChatMessage::to_bin()
     memset(_data, 0, MESSAGE_SIZE);
 
     //Serializar los campos type, nick y message en el buffer _data
+    char *tmp = _data;
+    //Copiar tipo a partir de direccion
+    memcpy(tmp, &type, sizeof(uint8_t));
+    tmp += sizeof(uint8_t);
+    //Copiar nick a partir de direccion
+    memcpy(tmp, &nick, sizeof(std::string));
+    tmp += sizeof(std::string);
+    //Copiar message a partir de direccion
+    memcpy(tmp, &message, sizeof(std::string));
 }
 
-int ChatMessage::from_bin(char * bobj)
+int ChatMessage::from_bin(char *bobj)
 {
     alloc_data(MESSAGE_SIZE);
 
     memcpy(static_cast<void *>(_data), bobj, MESSAGE_SIZE);
 
     //Reconstruir la clase usando el buffer _data
-
+    char *tmp = _data;
+    //Copiar tipo a partir de direccion
+    memcpy(&type, tmp, sizeof(uint8_t));
+    tmp += sizeof(uint8_t);
+    //Copiar nick a partir de direccion
+    nick=tmp;
+    tmp += sizeof(std::string);
+    //Copiar message a partir de direccion
+    message=tmp;
     return 0;
 }
 
@@ -72,10 +89,9 @@ void ChatClient::input_thread()
 
 void ChatClient::net_thread()
 {
-    while(true)
+    while (true)
     {
         //Recibir Mensajes de red
         //Mostrar en pantalla el mensaje de la forma "nick: mensaje"
     }
 }
-
